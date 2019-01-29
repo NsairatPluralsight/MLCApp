86dba1e152d489d6be4792840176b62cd6b7c04d
Mohammad Nsairat <mkmnsseirat@gmail.com>
2019-01-29 17:59:04 +0200

    add config for e2e test

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -3,13 +3,21 @@
 
 const { SpecReporter } = require('jasmine-spec-reporter');
 
+process.env.HEADLESS = process.env.HEADLESS || require("puppeteer").executablePath();
+
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    chromeOptions: {
+      args: process.env.HEADLESS
+        ? ['--headless', '--no-sandbox', '--disable-dev-shm-usage']
+        : [],
+      binary: process.env.HEADLESS ? require("puppeteer").executablePath() : undefined,
+    },
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
@@ -17,12 +25,20 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () { }
   },
   onPrepare() {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
+  },
+  plugins: [
+    {
+      package: "protractor-console-plugin",
+      failOnWarning: false,
+      failOnError: true,
+      logWarnings: true
+    }
+  ]
 };
