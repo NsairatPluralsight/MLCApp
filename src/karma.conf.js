389a7b4957a7c581fbd2cc4d23012794f08abb13
Mohammad Nsairat <mkmnsseirat@gmail.com>
2019-01-29 17:11:13 +0200

    test config

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,8 +1,5 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-const puppeteer = require('puppeteer');
-process.env.CHROME_BIN = puppeteer.executablePath();
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -13,8 +10,6 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-junit-reporter'),
-      require('karma-phantomjs-launcher')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -24,7 +19,7 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml', 'junit'],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
@@ -42,9 +37,5 @@ module.exports = function (config) {
       }
     },
     singleRun: true,
-    junitReporter: {
-      outputDir: '../junitResults',
-      suite: 'models'
-  }
   });
 };
