d683b17b311907a74e86441526dfaa1cd75bf656
Mohammad Nsairat <mkmnsseirat@gmail.com>
2019-01-29 10:11:39 +0200

    fix dublicates

--- src/app/shared/models/service.ts
+++ src/app/shared/models/service.ts
@@ -1,15 +1,8 @@
-export class Service {
-  id: number;
-  nameL1: string;
-  nameL2: string;
-  nameL3: string;
-  nameL4: string;
+import { BaseObjectEntity } from "./base-object-entity";
+
+export class Service extends BaseObjectEntity {
 
   constructor(id: number, nameL1: string, nameL2: string, nameL3: string, nameL4: string) {
-    this.id = id;
-    this.nameL1 = nameL1;
-    this.nameL2 = nameL2;
-    this.nameL3 = nameL3;
-    this.nameL4 = nameL4;
+    super(id, nameL1, nameL2, nameL3, nameL4);
   }
 }
